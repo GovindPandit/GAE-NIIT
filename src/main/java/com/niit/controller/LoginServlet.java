31b2b5c9aa1a93112494fd18ea4792648b0db6da
acer <acer@DESKTOP-EM79KRN>
2019-11-12 10:50:38 +0530

    //

--- src/main/java/com/niit/controller/LoginServlet.java
+++ src/main/java/com/niit/controller/LoginServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.niit.email.Email;
 import com.niit.model.User;
-
+//
 @WebServlet("/LoginController")
 public class LoginServlet extends HttpServlet 
 {
